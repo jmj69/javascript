8286aa0a9b2a76e6504d2a64e05ac0d3e7a7e9e9
JM <jm.jacquier@gmail.com>
2022-09-02 16:48:52 +0200

    compteur

--- modele/script.js
+++ modele/script.js
@@ -1,1 +1,24 @@
-console.log("hello");
+const btnRed = document.getElementById("btn-red");
+const btnBlue = document.getElementById("btn-blue");
+
+let compteur = 0;
+let titre = document.getElementById(".titre");
+
+btnRed.addEventListener("click", function () {
+  add();
+});
+
+btnBlue.addEventListener("click", function () {
+  add();
+});
+
+function add() {
+  compteur = compteur + 1;
+  console.log(compteur, "compteur");
+  titre.innerText = compteur;
+}
+
+setTimeout(function () {
+  btnBlue.remove();
+  btnRed.remove();
+}, 10000);
